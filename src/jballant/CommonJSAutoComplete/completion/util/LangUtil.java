c3a3093b73eed1b6e3b57036023f4020a5e3e781
Jamie Ballantine <jballant@gmail.com>
2015-07-19 13:40:17 -0400

    Updates for plugin for builds since 139

--- src/jballant/CommonJSAutoComplete/completion/util/LangUtil.java
+++ src/jballant/CommonJSAutoComplete/completion/util/LangUtil.java
@@ -1,7 +1,7 @@
 package jballant.CommonJSAutoComplete.completion.util;
 
-//import com.intellij.json.JsonFileType;
-import com.intellij.lang.javascript.json.JSONFileType;
+import com.intellij.json.JsonFileType;
+//import com.intellij.lang.javascript.json.JSONFileType;
 
 import com.intellij.lang.Language;
 import com.intellij.lang.javascript.JavaScriptFileType;
@@ -41,8 +41,8 @@ public class LangUtil {
     }
 
     public static boolean isJSONFileType(@Nullable FileType fileType) {
-//        return fileType != null && fileType.equals(JsonFileType.INSTANCE);
-        return fileType != null && fileType.equals(JSONFileType.JSON);
+        return fileType != null && fileType.equals(JsonFileType.INSTANCE);
+//        return fileType != null && fileType.equals(JSONFileType.JSON);
     }
 
     public static boolean isCoffeeScript (@NotNull Language lang) {
